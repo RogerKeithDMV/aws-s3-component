cbe87f8a5ec73ee7bd54ee174c29e8945e53a100
Daniel Maturano Vazquez <101372967+RogerKeithDMV@users.noreply.github.com>
2022-09-13 17:42:02 -0500

    init

---
+++ test/test.js
@@ -0,0 +1,33 @@
+const {log} = require('utils-nxg-cg');
+const {awss3,objectAWSS3Opt,objectAWSS3Req} = require('aws-s3-cg-lib');
+const express = require('express');
+
+const { request } = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.post('/', async(req, res)=>{
+  let properties = {...objectAWSS3Req};  
+    properties.flag = req.body.flag;
+    properties.region = req.body.region;
+    properties.bucketName = req.body.bucketName;
+    properties.fileName = req.body.fileName;
+    properties.content = req.body.content;
+    properties.accessKey = req.body.accessKey;
+    properties.secretKey = req.body.secretKey;
+
+    try{
+      const result = await awss3({data:properties},{});
+      log.info("resultado", result);
+      res.json(result);
+    }
+
+    catch(err){
+      res.status(500).json(err);
+    }
+  })
+
+  app.listen(3000, ()=>{
+    console.log("Server ejecutandose en el puerto 3000");
+  });
